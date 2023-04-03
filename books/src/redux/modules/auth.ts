f9e9df96f4310ece16c9d9f4f4f1fe2763d53eb1
pangkyu <baesense@naver.com>
2023-04-04 00:01:45 +0900

    Feat : 리덕스 스토어 설정
    redux, react-redex, redux-actions

---
+++ books/src/redux/modules/auth.ts
@@ -0,0 +1,48 @@
+import { createActions, handleActions } from "redux-actions";
+
+interface AuthState {
+  token: string | null;
+  loading: boolean;
+  error: Error | null;
+}
+const initialState: AuthState = {
+  token: null,
+  loading: false,
+  error: null,
+};
+
+const prefix = "my-books/auth";
+
+export const { pending, success, fail } = createActions(
+  "PENDING",
+  "SUCCESS",
+  "FAIL",
+  { prefix }
+);
+
+const reducer = handleActions<AuthState, string>(
+  {
+    PENDING: (state) => ({
+      ...state,
+      loading: true,
+      error: null,
+    }),
+    SUCCESS: (state, action) => ({
+      token: action.payload,
+      loading: false,
+      error: null,
+    }),
+    FAIL: (state, action: any) => ({
+      ...state,
+      loading: false,
+      error: action.payload,
+    }),
+  },
+  initialState,
+  { prefix }
+);
+
+export default reducer;
+
+//saga
+export function* authSaga() {}
