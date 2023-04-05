2a7e9d7ed55ae6fdacaa8947bc1678d9a58b6212
pangkyu <baesense@naver.com>
2023-04-05 12:48:19 +0900

    Feat : useRef 적용

--- books/src/components/Signin.tsx
+++ books/src/components/Signin.tsx
@@ -1,7 +1,13 @@
 import { Row, Col, Input, Button } from "antd";
 import styles from "./Signin.module.css";
+import { useRef } from "react";
 
-export default function Signin() {
+interface SigninProps {}
+
+const Signin: React.FC<SigninProps> = () => {
+  const emailRef = useRef<any>(null);
+  const passwordRef = useRef<any>(null);
+
   return (
     <Row align="middle" className={styles.signin_row}>
       <Col span={24}>
@@ -23,10 +29,11 @@ export default function Signin() {
             </div>
             <div className={styles.input_area}>
               <Input
-                className={styles.input}
                 placeholder="Email"
                 autoComplete="email"
                 name="email"
+                className={styles.input}
+                ref={emailRef}
               />
             </div>
             <div className={styles.password_title}>
@@ -37,12 +44,16 @@ export default function Signin() {
               <Input
                 type="password"
                 autoComplete="current-password"
-                name="email"
                 className={styles.input}
+                ref={passwordRef}
               />
             </div>
             <div className={styles.button_area}>
-              <Button size="large" className={styles.button}>
+              <Button
+                size="large"
+                className={styles.button}
+                onClick={handleClick}
+              >
                 Sign In
               </Button>
             </div>
@@ -51,4 +62,11 @@ export default function Signin() {
       </Col>
     </Row>
   );
-}
+
+  function handleClick() {
+    const email = emailRef.current!.state.value;
+    const password = passwordRef.current!.state.value;
+  }
+};
+
+export default Signin;
